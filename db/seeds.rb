ca9301b6829ea2cded4bb1bb2972afd1260ebd1e
JustinLC <Justin_L.C@hotmail.com>
2016-03-28 06:16:37 +0000

    Major bootstrap changes final

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,12 @@ user = User.create!(
 20.times do
   tripname = Faker::StarWars.planet
   destination = Faker::Address.country
+  startdate = Faker::Date.between(Date.today, 100.days.from_now)
+  enddate = Faker::Date.between(101.days.from_now, 200.days.from_now)
   user.trips.create!(
     tripname: tripname,
     destination: destination,
+    startdate: startdate,
+    enddate: enddate
   )
 end
